da65a85d4abff4991c92a6505796cb6609d54ddf
Ayaan Adrito <ayaan.adrito@gmail.com>
2025-01-28 11:26:57 -0500

    Add elevator constants

--- include/globals.h
+++ include/globals.h
@@ -5,3 +5,7 @@ const int RIGHT_MOTOR_TWO_ID = 1;
 
 const int LEFT_MOTOR_ONE_ID = 19;
 const int LEFT_MOTOR_TWO_ID = 10;
+
+const int ELEVATOR_MOTOR_TRACK_ID = 1
+const int ELEVATOR_MOTOR_EXTENDOR_ID = 2;
+const int ELEVATOR_SPEED = 10;
--- src/drivetrain-functions.cpp
+++ src/drivetrain-functions.cpp
@@ -2,22 +2,14 @@
 #include "globals.h"
 #include "main.h"
 
-
-
-
-
 pros::MotorGroup left_motors({LEFT_MOTOR_ONE_ID, LEFT_MOTOR_TWO_ID});
 pros::MotorGroup right_motors({RIGHT_MOTOR_ONE_ID, RIGHT_MOTOR_TWO_ID});
-
-
-
 
 void move(int speed) {
     right_motors.move(-speed);
     left_motors.move(speed);
     
 }
-
 
 void turn(int speed) {
     right_motors.move(speed);
