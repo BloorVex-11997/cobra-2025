86cdf8a01c6b1b56d8ab82972506faa32891761e
Ayaan Adrito <ayaan.adrito@gmail.com>
2025-01-29 13:03:39 -0500

    Reconfigure wheel motor ports constants

--- include/globals.hpp
+++ include/globals.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
-const int RIGHT_MOTOR_ONE_ID = 12;
-const int RIGHT_MOTOR_TWO_ID = 1;
-
-const int LEFT_MOTOR_ONE_ID = 19;
-const int LEFT_MOTOR_TWO_ID = 10;
+const int RIGHT_FRONT_MOTOR_ID = 15;
+const int LEFT_FRONT_MOTOR_ID = 16;
+const int RIGHT_BACK_MOTOR_ID = 5;
+const int LEFT_BACK_MOTOR_ID = 9;
 
 const int ELEVATOR_MOTOR_ONE_ID = 1;
 const int ELEVATOR_MOTOR_TWO_ID = 1;
