c92a1283d73749c5518328b13fbc87447414342e
Ayaan Adrito <ayaan.adrito@gmail.com>
2025-01-30 22:49:43 -0500

    Modularize drivetrain-function.cpp

--- src/subsystems/drivetrain-functions.cpp
+++ src/subsystems/drivetrain-functions.cpp
@@ -2,14 +2,16 @@
 #include "drivetrain-functions.hpp"
 #include "globals.hpp"
 
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+pros::MotorGroup left_motors({LEFT_FRONT_MOTOR_ID, LEFT_BACK_MOTOR_ID});
+pros::MotorGroup right_motors({RIGHT_FRONT_MOTOR_ID, RIGHT_BACK_MOTOR_ID});
 
 double drive_multiplier = 1;
 bool precision_drive = false;
 bool turbo_drive = false;
 
-
-pros::MotorGroup left_motors({LEFT_FRONT_MOTOR_ID, LEFT_BACK_MOTOR_ID});
-pros::MotorGroup right_motors({RIGHT_FRONT_MOTOR_ID, RIGHT_BACK_MOTOR_ID});
+int analogY = 0;
+int analogX = 0;
 
 void move(int speed) {
 	// WARNING: DON'T CHANGE SIGNS BECAUSE THE MOTORS ARE INVERTED!
@@ -23,29 +25,32 @@ void turn(int speed) {
     
 }
 
+void handle_input() {
+    analogY = controller.get_analog(ANALOG_LEFT_Y);  // Forward/backward
+    analogX = controller.get_analog(ANALOG_RIGHT_X); // Turning
+
+    precision_drive = controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1);
+    turbo_drive = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
+
+    if (precision_drive) {
+            drive_multiplier = PRECISION_MULTIPLIER;
+    } else if (turbo_drive) {
+            drive_multiplier = TURBO_MULTIPLIER;
+    } else {
+        drive_multiplier = NORMAL_MULTIPLIER;
+    }
+}
+
 void drivetrain_periodic() {
-    pros::Controller controller(pros::E_CONTROLLER_MASTER);
     
 	while(true){
-		int analogY = controller.get_analog(ANALOG_LEFT_Y);  // Forward/backward
-        int analogX = controller.get_analog(ANALOG_RIGHT_X); // Turning
-
-        precision_drive = controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1);
-        turbo_drive = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
-
-        if (precision_drive) {
-            drive_multiplier = PRECISION_MULTIPLIER;
-        } else if (turbo_drive) {
-            drive_multiplier = TURBO_MULTIPLIER;
-        } else {
-            drive_multiplier = NORMAL_MULTIPLIER;
-        }
+		handle_input();
 
         // Allow moving and turning simultaneously
         int left_speed = static_cast<int>((analogY + analogX) * drive_multiplier);
         int right_speed = static_cast<int>((analogY - analogX) * drive_multiplier);
 
         left_motors.move(left_speed);
-        right_motors.move(-right_speed); // Inverted due to motor orientation
+        right_motors.move(-right_speed); // Inverted due to motor orientation (DO NOT CHANGE!)
 	}
 }
