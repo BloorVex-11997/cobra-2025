3d6e91b785252268f098a076a9e8840abb61d12e
Ayaan Adrito <ayaan.adrito@gmail.com>
2025-01-31 16:06:27 -0500

    Fix tank drive

--- src/subsystems/drivetrain-functions.cpp
+++ src/subsystems/drivetrain-functions.cpp
@@ -11,8 +11,12 @@ double drive_multiplier = 1;
 bool precision_drive = false;
 bool turbo_drive = false;
 
-int analogY = 0;
-int analogX = 0;
+// Tank drive
+int analogRightY = 0;
+
+// Arcade drive
+int analogLeftY = 0;
+int analogRightX = 0;
 
 void move(int left_speed, int right_speed) {
 	// WARNING: DON'T CHANGE SIGNS BECAUSE THE MOTORS ARE INVERTED!
@@ -21,8 +25,9 @@ void move(int left_speed, int right_speed) {
 }
 
 void handle_input() {
-    analogY = controller.get_analog(ANALOG_LEFT_Y);  // Forward/backward
-    analogX = controller.get_analog(ANALOG_RIGHT_X); // Turning
+    analogRightY = controller.get_analog(ANALOG_RIGHT_Y);
+    analogLeftY = controller.get_analog(ANALOG_LEFT_Y);  // Forward/backward
+    analogRightX = controller.get_analog(ANALOG_RIGHT_X); // Turning
 
     precision_drive = controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1);
     turbo_drive = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
@@ -51,15 +56,15 @@ void drivetrain_periodic() {
 
         if(arcade_drive){
             // Arcade drive
-            int left_speed = static_cast<int>((analogY + analogX) * drive_multiplier);
-            int right_speed = static_cast<int>((analogY - analogX) * drive_multiplier);
+            int left_speed = static_cast<int>((analogLeftY + analogRightX) * drive_multiplier);
+            int right_speed = static_cast<int>((analogLeftY - analogRightX) * drive_multiplier);
 
             move(left_speed, right_speed);
 
         } else {
             // Tank drive
-            int left_speed = static_cast<int>(analogY * drive_multiplier);
-            int right_speed = static_cast<int>(analogX * drive_multiplier);
+            int left_speed = static_cast<int>(analogLeftY * drive_multiplier);
+            int right_speed = static_cast<int>(analogRightY * drive_multiplier);
 
             move(left_speed, right_speed);
         }
