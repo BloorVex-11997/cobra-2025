d8f39ca1740383f59eddca68c664670be5f9d1ee
Ayaan Adrito <ayaan.adrito@gmail.com>
2025-02-01 20:02:52 -0500

    Finish elevator functions

--- include/globals.hpp
+++ include/globals.hpp
@@ -5,8 +5,7 @@ const int LEFT_FRONT_MOTOR_ID = 16;
 const int RIGHT_BACK_MOTOR_ID = 5;
 const int LEFT_BACK_MOTOR_ID = 6;
 
-const int ELEVATOR_MOTOR_ONE_ID = 1;
-const int ELEVATOR_MOTOR_TWO_ID = 1;
+const int ELEVATOR_MOTOR_ID = 1;
 const int ELEVATOR_SPEED = 10;
 
 const int GRABBER_MOTOR_ID = 1;
--- src/subsystems/elevator-functions.cpp
+++ src/subsystems/elevator-functions.cpp
@@ -3,17 +3,27 @@
 #include "globals.hpp"
 #include "subsystems/elevator-functions.hpp"
 
-pros::Motor elevator_motor_one(ELEVATOR_MOTOR_ONE_ID);
-pros::Motor elevator_motor_two(ELEVATOR_MOTOR_TWO_ID);
+pros::Motor elevator_motor(ELEVATOR_MOTOR_ID);
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
+bool active = false;
 
 void extend_elevator() {
-    elevator_motor_one.move(ELEVATOR_SPEED);
+    elevator_motor.move(ELEVATOR_SPEED);
 }
 
 void retract_elevator() {
-    elevator_motor_one.move(-ELEVATOR_SPEED);
+    elevator_motor.move(-ELEVATOR_SPEED);
 }
 
 void elevator_periodic() {
-    
+    if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_X)) {
+        active = !active;
+    }
+
+    if(active){
+        extend_elevator();
+    } else {
+        retract_elevator();
+    }
 }
